70ff1ee8471ac287c58905424a62b90f1ee04ced
Wesley Berrêdo <wberredo@gmail.com>
2023-05-08 11:53:29 -0300

    Update the script to create a CSV with the embeddings and the number of tokens

--- lib/create_embeddings.rb
+++ lib/create_embeddings.rb
@@ -1,13 +1,27 @@
-filename = ARGV[0]
+embedding_model = "text-search-curie-doc-001"
 
+filename = ARGV[0]
 reader = PDF::Reader.new(filename)
 
-CSV.open("#{filename}_embeddings.csv", "w") do |csv|
-  csv << ["title", "content"]
+CSV.open("book_embeddings.csv", "w") do |csv|
+  csv << ["title", "content", "total_tokens", *(0..4095).to_a]
 
   reader.pages.each_with_index do |page, index|
     if not page.text.blank?
-      csv << ["Page #{index + 1}", page.text]
+      response = HTTP
+        .headers(:accept => "application/json")
+        .auth("Bearer #{ENV['OPENAI_API_KEY']}")
+        .post("https://api.openai.com/v1/embeddings", :json => {
+            :model => embedding_model,
+            :input => page.text,
+        })
+        .body
+      result = JSON.parse(response);
+
+      embeddings = result["data"][0]["embedding"]
+      tokens = result["usage"]["total_tokens"]
+
+      csv << ["Page #{index + 1}", page.text, tokens, *embeddings]
     end
   end
 end
