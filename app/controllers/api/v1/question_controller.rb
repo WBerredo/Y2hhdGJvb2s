7e4d43bf940aea4973f6fb9e1579153cf8c60fca
Wesley Berrêdo <wberredo@gmail.com>
2023-05-08 21:56:25 -0300

    Create Question model and use it as a cache for used questions

--- app/controllers/api/v1/question_controller.rb
+++ app/controllers/api/v1/question_controller.rb
@@ -8,6 +8,11 @@ class Api::V1::QuestionController < ApplicationController
             question += "?"
         end
 
+        db_question = Question.find_by(question: params[:question])
+        if db_question
+            return render json: {answer: db_question.answer}
+        end
+
         question_embeddings = Numo::NArray[*helpers.generate_embeddings(question)]
 
         csv_table = CSV.read("book_embeddings.csv", headers: true, return_headers: false).map(&:fields)
@@ -24,6 +29,9 @@ class Api::V1::QuestionController < ApplicationController
         result = helpers.request_completion(prompt)
         answer = result["choices"][0]["text"]
 
+        question_to_be_saved = Question.create(question: question, answer: answer, context: prompt)
+        question.save
+        
         render json: {answer: answer}
     end
 end
